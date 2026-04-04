d25728c89fbc54f7920f1089eb1212afef270c8d
jojo <jojosbytes@gmail.com>
2026-04-04 13:00:35 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    return res.status(200).json({ number: 12345 });
-  }
-
-  return res.status(200).send("Nothing to see here.");
-}
